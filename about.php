1c725f966ad0f97676ecc48f358bb62116356418
Austin Persichetti <austinmpersichetti@gmail.com>
2023-03-30 14:47:09 -0600

    Forgot my last name

--- about.php
+++ about.php
@@ -14,7 +14,7 @@
     <div class="card">
             <?php echo Asset::img('IMG_0196.JPG', array('style' => 'width:100%')) ?>
             <div class="container">
-                <h4><b>Austin</b></h4> 
+                <h4><b>Austin Persichetti</b></h4> 
                 <p>My name is Austin Persichetti, I am a third year Colorado State University student studying Computer Science with a minor in Business Administration. My main goals are to better myself and learn the most I can to prepare for my future careeer.<p> 
             </div>
         </div>
