1eacdaaab6bf57e307f546d6f49e1fc7689be269
JamesZSilva <114622194+JamesZSilva@users.noreply.github.com>
2023-03-29 18:17:27 -0600

    Create about.php

---
+++ about.php
@@ -0,0 +1,33 @@
+<body>
+<h2>About Page</h2>
+<div class = "row">
+    <div class = "column">
+    <div class="card">
+            <?php echo Asset::img('IMG_0196.JPG', array('style' => 'width:100%')) ?>
+            <div class="container">
+                <h4><b>Jaimuel Silva</b></h4> 
+                <p>My name is Jaimuel Silva and I am studying computer science at Colorado State University. My goal is to develop a software that will impact the technology world. My biggest achievement was being able to tear down a car engine and put it back together with no issues.</p> 
+            </div>
+        </div>
+    </div>
+    <div class = "column">
+    <div class="card">
+            <?php echo Asset::img('IMG_0196.JPG', array('style' => 'width:100%')) ?>
+            <div class="container">
+                <h4><b>Austin</b></h4> 
+                <p>My name is Jaimuel Silva and I am studying computer science at Colorado State University. My goal is to develop a software that will impact the technology world. My biggest achievement was being able to tear down a car engine and put it back together with no issues.</p> 
+            </div>
+        </div>
+    </div>
+    <div class = "column">
+    <div class="card">
+            <?php echo Asset::img('IMG_0196.JPG', array('style' => 'width:100%')) ?>
+            <div class="container">
+                <h4><b>Glenn</b></h4> 
+                <p>My name is Jaimuel Silva and I am studying computer science at Colorado State University. My goal is to develop a software that will impact the technology world. My biggest achievement was being able to tear down a car engine and put it back together with no issues.</p> 
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html> 
