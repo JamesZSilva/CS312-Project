cd2b9004c924a223403c6b4767d0e77ba8632e84
JamesZSilva <114622194+JamesZSilva@users.noreply.github.com>
2023-03-29 18:18:17 -0600

    Create table.php

---
+++ table.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>
+</title>
+</head>
+<body>
+<h1> Homepage </h1>
+<?php echo Asset::img("IMG_0196.JPG")?>
+<h2> Biography </h2>
+<ul>
+<li> My name is Jaimuel Silva but you can also call me James! </li>
+<li> I love working on cars and also watching sports! </li>
+<li> Currently I am at CSU studying computer Science! </li>
+</ul>   
+</body>
+</html>
