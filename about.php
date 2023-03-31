07f853ce271bd0dc3912b8758ca91af8aef5d833
AustinSketti <110070395+AustinSketti@users.noreply.github.com>
2023-03-31 16:54:28 -0600

    Update about.php

--- about.php
+++ about.php
@@ -12,7 +12,7 @@
     </div>
     <div class = "column">
     <div class="card">
-            <?php echo Asset::img('IMG_0196.JPG', array('style' => 'width:100%')) ?>
+            <?php echo Asset::img('Austin-Picture.JPG', array('style' => 'width:100%')) ?>
             <div class="container">
                 <h4><b>Austin Persichetti</b></h4> 
                 <p>My name is Austin Persichetti, I am a third year Colorado State University student studying Computer Science with a minor in Business Administration. My main goals are to better myself and learn the most I can to prepare for my future careeer.<p> 
