07b3eadcbd246ec8f3da743b4dd2c34eed52b0dc
JamesZSilva <114622194+JamesZSilva@users.noreply.github.com>
2023-03-31 17:23:10 -0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -6,12 +6,6 @@
 </head>
 <body>
 <h1> Homepage </h1>
-<?php echo Asset::img("IMG_0196.JPG")?>
-<h2> Biography </h2>
-<ul>
-<li> My name is Jaimuel Silva but you can also call me James! </li>
-<li> I love working on cars and also watching sports! </li>
-<li> Currently I am at CSU studying computer Science! </li>
-</ul>   
+<?php echo Asset::img('csuimage.webp', array('style' => 'width:100%')) ?>
 </body>
 </html>
