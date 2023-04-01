f73d7a0629c58df1c202bd3bd6720137e214ba87
AustinSketti <110070395+AustinSketti@users.noreply.github.com>
2023-03-31 18:58:15 -0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE HTML>
 <html>
+    <style>
+       body {text-align: center;}
+       p {font-size: 30px;
+          text-align: left;}
+    </style>
 <head>
 <title>
 </title>
 </head>
 <body>
 <h1> Homepage </h1>
-<?php echo Asset::img('csuimage.webp', array('style' => 'width:100%')) ?>
+<?php echo Asset::img('CC-Logo.png', array('style' => 'width:20%')) ?>
+<ul style="list-style-type: disc;">
+    <li style="color:white;"><p>Group of three students known as the "Coding Crew" working to better themsevles and develop a website using the Fuel framework</p></li>
+    <li style="color:white;"><p>To learn more about the developers press the "About" button at the top</p></li>
+    <li style="color:white;"><p>To visit our Color Coordinate Generation press the "Table" button.</p></li>
+</ul>
 </body>
 </html>
