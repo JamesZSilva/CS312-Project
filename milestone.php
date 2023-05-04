639fbc34a6732f0e23df8cfe2c96982057a9e4c2
AustinSketti <110070395+AustinSketti@users.noreply.github.com>
2023-05-04 14:28:01 -0600

    Update milestone.php

--- milestone.php
+++ milestone.php
@@ -57,61 +57,82 @@ class Controller_milestone extends Controller_Template
         $row_value = Input::post('rowvalue');
         $color_value = Input::post('colorvalue');
 
-        // Create color table
-        $colors = array('red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'gray', 'teal');
-        $color_table = '<table class="color-table" style="border-collapse: collapse; border: 1px solid black; width: 80%;">';
-        $color_selects = array();
-        for ($i = 0; $i < $color_value; $i++) {
-            $color = $colors[$i % count($colors)];
-            $color_selects[] = $color;
-            $color_table .= '<tr>';
-            $color_table .= '<td style="border: 1px solid black; width: 20%;"><select class="color-dropdown" data-index="' . $i . '">';
-            foreach ($colors as $c) {
-                $selected = $c == $color ? 'selected' : '';
-                $color_table .= '<option value="' . $c . '" ' . $selected . '>' . ucfirst($c) . '</option>';
-            }
-            $color_table .= '</select></td>';
-            $color_table .= '<td class="color-cell" style="border: 1px solid black; width: 80%; background-color:' . $color . ';">&nbsp;</td>';
-            $color_table .= '</tr>';
-        }
-        $color_table .= '</table>';
+       $colors = array('red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'gray', 'teal');
+       $color_table = '<table class="color-table" style="border-collapse: collapse; border: 1px solid black; width: 80%;">';
+       $color_selects = array();
+       for ($i = 0; $i < $color_value; $i++) {
+           $color = $colors[$i % count($colors)];
+           $color_selects[] = $color;
+		   $currId = 'color'.$i;
+           $color_table .= '<tr>';
+           $color_table .= '<td style="border: 1px solid black; width: 20%;"><select class="color-dropdown" data-index="' . $i . '">';
+           foreach ($colors as $c) {
+               $selected = $c == $color ? 'selected' : '';
+               $color_table .= '<option value="' . $c . '" ' . $selected . '>' . ucfirst($c) . '</option>';
+           }
+           $color_table .= '</select></td>';
+           $color_table .= '<td id="'.$currId.'" class="'.$color.'" style="border: 1px solid black; width: 80%; background-color:' . $color . ';">&nbsp;</td>';
+           $color_table .= '</tr>';
+       }
+       $color_table .= '</table>';
 
-        // Create normal table
-        $normal_table = '<table class="normal-table" style="border-collapse: collapse; border: 1px solid black; width: 50%;">';
-        for ($i = 1; $i <= $row_value+1; $i++) {
-            $normal_table .= '<tr>';
-            for ($j = 1; $j <= $row_value+1; $j++) {
-                if ($i == 1 && $j == 1) {
-                    $normal_table .= '<td></td>';
-                } else if ($i == 1) {
-                    $normal_table .= '<td>' . chr($j+63) . '</td>';
-                } else if ($j == 1) {
-                    $normal_table .= '<td>' . ($i-1) . '</td>';
-                } else {
-                    $normal_table .= '<td>' . ($i-1)*($j-1) . '</td>';
-                }
-            }
-            $normal_table .= '</tr>';
-        }
-        $normal_table .= '</table>';
+		echo $color_table;
 
-        echo $color_table;
-        echo "<br></br>";
-        echo '<div style="text-align:center;">'.$normal_table.'</div>';
-		echo '<div style="text-align:center;"><button id="print-button">Printable View</button></div>';
-		echo '<script>
-			document.querySelectorAll(".color-dropdown").forEach(function(dropdown) {
-				dropdown.addEventListener("change", function() {
-					var colorCell = this.parentNode.nextElementSibling;
-					var newColor = this.value;
-					if (newColor !== colorCell.style.backgroundColor) {
-						colorCell.style.backgroundColor = newColor;
-					} else {
-						this.value = colorCell.style.backgroundColor;
-					}
-				});
+       // Create normal table
+       $normal_table = '<table class="normal-table" style="border-collapse: collapse; border: 1px solid black; width: 50%;">';
+       for ($i = 1; $i <= $row_value+1; $i++) {
+           $normal_table .= '<tr>';
+           for ($j = 1; $j <= $row_value+1; $j++) {
+               if ($i == 1 && $j == 1) {
+                   $normal_table .= '<td></td>';
+               } else if ($i == 1) {
+                   $normal_table .= '<td>' . chr($j+63) . '</td>';
+               } else if ($j == 1) {
+                   $normal_table .= '<td>' . ($i-1) . '</td>';
+               } else {
+                   $normal_table .= '<td id="cell' . $i . ',' . $j . '" class="myCell">' . ($i-1).','.($j-1) . '</td>';
+               }
+           }
+           $normal_table .= '</tr>';
+       }
+       $normal_table .= '</table>';
+
+	   $normal_table .= '<script>
+		var colorCells = document.getElementsByClassName("color-table")[0].getElementsByTagName("td");
+		var selectedColor = "";
+
+		for (var i = 0; i < colorCells.length; i++) {
+			colorCells[i].addEventListener("click", function() {
+				selectedColor = this.style.backgroundColor;
 			});
+		}
+
+		var normalCells = document.getElementsByClassName("normal-table")[0].getElementsByTagName("td");
+
+		for (var i = 0; i < normalCells.length; i++) {
+			normalCells[i].addEventListener("click", function() {
+				this.style.backgroundColor = selectedColor;
+			});
+		}
 		</script>';
+
+
+       echo "<br></br>";
+       echo '<div style="text-align:center;">'.$normal_table.'</div>';
+       echo '<div style="text-align:center;"><button id="print-button">Printable View</button></div>';
+       echo '<script>
+       document.querySelectorAll(".color-dropdown").forEach(function(dropdown) {
+           dropdown.addEventListener("change", function() {
+               var colorCell = this.parentNode.nextElementSibling;
+               var newColor = this.value;
+               if (newColor !== colorCell.style.backgroundColor) {
+                   colorCell.style.backgroundColor = newColor;
+               } else {
+                   this.value = colorCell.style.backgroundColor;
+               }
+           });
+       });
+       </script>';
     }
     $this->template->content = View::forge('milestone/rowfunction', $data);
 }
