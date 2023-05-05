d8ad08f9e4b75d826528158d1d1159c96a344da6
GMCINTO <89421618+GMCINTO@users.noreply.github.com>
2023-05-05 14:28:10 -0600

    Update milestone.php

--- milestone.php
+++ milestone.php
@@ -1,6 +1,4 @@
 <?php
-
-
 /**
  * The Welcome Controller.
  *
@@ -12,140 +10,156 @@
  */
 class Controller_milestone extends Controller_Template
 {
-	public $direction = "";
-	public $direction2 = "";
-	public $direction3 = "";
-
-	/**
-	 * The basic welcome message
-	 *
-	 * @access  public
-	 * @return  Response
-	 */
-	public function action_index()
-	{
-		$data = array();
-		$this->template->title = 'Index Page Title';
-		$this->template->css = "m1.css";
-		$this->template->content = View::forge('milestone/Index',$data);
-	}
-
-	public function action_about(){
-		$data = array();
-		$this->template->title = 'About Page Title';
-		$this->template->css = "aboutpage.css";
-		$this->template->content = View::forge('milestone/about',$data);
-
-	}
-
-	public function action_table()
-	{
-		$data = array();
-		$this->template->title = 'Table Page Title';
-		$this->template->css = "m1.css";
-		$this->template->content = View::forge('milestone/table',$data);
-	}
-
-	public function action_rowfunction()
-{
-    $data = array();
-    $this->template->title = 'RowFunction Page Title';
-    $this->template->css = "table.css";
-
-    if (Input::method() == 'POST')
+    public $direction = "";
+    public $direction2 = "";
+    public $direction3 = "";
+    /**
+     * The basic welcome message
+     *
+     * @access  public
+     * @return  Response
+     */
+    public function action_index()
     {
-        $row_value = Input::post('rowvalue');
-        $color_value = Input::post('colorvalue');
-
-       $colors = array('red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'gray', 'teal');
-       $color_table = '<table class="color-table" style="border-collapse: collapse; border: 1px solid black; width: 80%;">';
-       $color_selects = array();
-       for ($i = 0; $i < $color_value; $i++) {
-           $color = $colors[$i % count($colors)];
-           $color_selects[] = $color;
-		   $currId = 'color'.$i;
-           $color_table .= '<tr>';
-           $color_table .= '<td style="border: 1px solid black; width: 20%;"><select class="color-dropdown" data-index="' . $i . '">';
-           foreach ($colors as $c) {
-               $selected = $c == $color ? 'selected' : '';
-               $color_table .= '<option value="' . $c . '" ' . $selected . '>' . ucfirst($c) . '</option>';
-           }
-           $color_table .= '</select></td>';
-           $color_table .= '<td id="'.$currId.'" class="'.$color.'" style="border: 1px solid black; width: 80%; background-color:' . $color . ';">&nbsp;</td>';
-           $color_table .= '</tr>';
-       }
-       $color_table .= '</table>';
-
-		echo $color_table;
-
-       // Create normal table
-       $normal_table = '<table class="normal-table" style="border-collapse: collapse; border: 1px solid black; width: 50%;">';
-       for ($i = 1; $i <= $row_value+1; $i++) {
-           $normal_table .= '<tr>';
-           for ($j = 1; $j <= $row_value+1; $j++) {
-               if ($i == 1 && $j == 1) {
-                   $normal_table .= '<td></td>';
-               } else if ($i == 1) {
-                   $normal_table .= '<td>' . chr($j+63) . '</td>';
-               } else if ($j == 1) {
-                   $normal_table .= '<td>' . ($i-1) . '</td>';
-               } else {
-                   $normal_table .= '<td id="cell' . $i . ',' . $j . '" class="myCell">' . ($i-1).','.($j-1) . '</td>';
-               }
-           }
-           $normal_table .= '</tr>';
-       }
-       $normal_table .= '</table>';
-
-	   $normal_table .= '<script>
-		var colorCells = document.getElementsByClassName("color-table")[0].getElementsByTagName("td");
-		var selectedColor = "";
-
-		for (var i = 0; i < colorCells.length; i++) {
-			colorCells[i].addEventListener("click", function() {
-				selectedColor = this.style.backgroundColor;
-			});
-		}
-
-		var normalCells = document.getElementsByClassName("normal-table")[0].getElementsByTagName("td");
-
-		for (var i = 0; i < normalCells.length; i++) {
-			normalCells[i].addEventListener("click", function() {
-				this.style.backgroundColor = selectedColor;
-			});
-		}
-		</script>';
-
-
-       echo "<br></br>";
-       echo '<div style="text-align:center;">'.$normal_table.'</div>';
-       echo '<div style="text-align:center;"><button id="print-button">Printable View</button></div>';
-       echo '<script>
-       document.querySelectorAll(".color-dropdown").forEach(function(dropdown) {
-           dropdown.addEventListener("change", function() {
-               var colorCell = this.parentNode.nextElementSibling;
-               var newColor = this.value;
-               if (newColor !== colorCell.style.backgroundColor) {
-                   colorCell.style.backgroundColor = newColor;
-               } else {
-                   this.value = colorCell.style.backgroundColor;
-               }
-           });
-       });
-       </script>';
+        $data = array();
+        $this->template->title = 'Index Page Title';
+        $this->template->css = "m1.css";
+        $this->template->content = View::forge('milestone/Index', $data);
     }
-    $this->template->content = View::forge('milestone/rowfunction', $data);
-}
-	
+    public function action_about()
+    {
+        $data = array();
+        $this->template->title = 'About Page Title';
+        $this->template->css = "m1.css";
+        $this->template->content = View::forge('milestone/about', $data);
+    }
+    public function action_table()
+    {
+        $data = array();
+        $this->template->title = 'Table Page Title';
+        $this->template->css = "m1.css";
+        $this->template->content = View::forge('milestone/table', $data);
+    }
+    public function action_rowfunction()
+    {
+        $data = array();
+        $this->template->title = 'RowFunction Page Title';
+        $this->template->css = "m1.css";
+        if (Input::method() == 'POST') {
+            $row_value = Input::post('rowvalue');
+            $color_value = Input::post('colorvalue');
+            // $colors = array('red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'gray', 'teal');
 
-	/**
-	 * The 404 action for the application.
-	 *
-	 * @access  public
-	 * @return  Response
-	 */
-	public function action_404()
-	{
-		return Response::forge(Presenter::forge('welcome/404'), 404);
-	}
+
+            $host = "sql9.freesqldatabase.com";
+            $user = "sql9616240";
+            $pass = "DJrWkD4P75";
+            $dbname = "sql9616240";
+
+            // Connect to the database
+            $conn = mysqli_connect($host, $user, $pass, $dbname);
+
+            if (!$conn) {
+                die("Could not connect to database: " . mysqli_error());
+            }
+
+            // Execute a SQL query
+            $sql = "SELECT name, hex_value FROM colors";
+            $result = mysqli_query($conn, $sql);
+
+            $colors = array(); // Initialize an empty array to store the colors
+
+            if (mysqli_num_rows($result) > 0) {
+                // Loop through the results and add each color to the array
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $color = array(
+                        "name" => $row["name"],
+                        "hex_value" => $row["hex_value"]
+                    );
+                    array_push($colors, $color);
+                }
+            }
+
+            // Close the database connection
+            mysqli_close($conn);
+
+            $color_table = '<table class="color-table" style="border-collapse: collapse; border: 1px solid black; width: 80%;">';
+            $color_selects = array();
+
+            foreach ($colors as $color) {
+                $color_selects[] = $color;
+                $currId = 'color_' . str_replace(' ', '_', strtolower($color['name']));
+                $color_table .= '<tr>';
+                $color_table .= '<td style="border: 1px solid black; width: 20%;"><select class="color-dropdown" data-index="' . $currId . '">';
+                foreach ($colors as $c) {
+                    $selected = $c['name'] == $color['name'] ? 'selected' : '';
+                    $color_table .= '<option value="' . $c['hex_value'] . '" ' . $selected . '>' . ucfirst($c['name']) . '</option>';
+                }
+                $color_table .= '</select></td>';
+                $color_table .= '<td id="' . $currId . '" class="' . $currId . '" style="border: 1px solid black; width: 80%; background-color:' . $color['hex_value'] . ';">&nbsp;</td>';
+                $color_table .= '</tr>';
+            }
+            $color_table .= '</table>';
+
+            echo $color_table;
+
+            // Create normal table
+            $normal_table = '<table class="normal-table" style="border-collapse: collapse; border: 1px solid black; width: 50%;">';
+            for ($i = 1; $i <= $row_value + 1; $i++) {
+                $normal_table .= '<tr>';
+                for ($j = 1; $j <= $row_value + 1; $j++) {
+                    if ($i == 1 && $j == 1) {
+                        $normal_table .= '<td></td>';
+                    } else if ($i == 1) {
+                        $normal_table .= '<td>' . chr($j + 63) . '</td>';
+                    } else if ($j == 1) {
+                        $normal_table .= '<td>' . ($i - 1) . '</td>';
+                    } else {
+                        $normal_table .= '<td id="cell' . $i . ',' . $j . '" class="myCell">' . ($i - 1) . ',' . ($j - 1) . '</td>';
+                    }
+                }
+                $normal_table .= '</tr>';
+            }
+            $normal_table .= '</table>';
+
+            $normal_table .= '<script>
+             var colorCells = document.getElementsByClassName("color-table")[0].getElementsByTagName("td");
+             var selectedColor = "";
+             for (var i = 0; i < colorCells.length; i++) {
+                 colorCells[i].addEventListener("click", function() {
+                     selectedColor = this.style.backgroundColor;
+                 });
+             }
+             var normalCells = document.getElementsByClassName("normal-table")[0].getElementsByTagName("td");
+             for (var i = 0; i < normalCells.length; i++) {
+                 normalCells[i].addEventListener("click", function() {
+                     this.style.backgroundColor = selectedColor;
+                 });
+             }
+             </script>';
+
+
+            echo "<br></br>";
+            echo '<div style="text-align:center;">' . $normal_table . '</div>';
+            echo '<div style="text-align:center;"><button onclick="display()">Click to Print</button></div>';
+            echo '<script>
+            document.querySelectorAll(".color-dropdown").forEach(function(dropdown) {
+                dropdown.addEventListener("change", function() {
+                    var colorCell = this.parentNode.nextElementSibling;
+                    var newColor = this.value;
+                    if (newColor !== colorCell.style.backgroundColor) {
+                        colorCell.style.backgroundColor = newColor;
+                    } else {
+                        this.value = colorCell.style.backgroundColor;
+                    }
+                });
+            });
+            function display() {
+                window.print();
+             }
+            </script>';
+
+        }
+        $this->template->content = View::forge('milestone/rowfunction', $data);
+    }
 }
