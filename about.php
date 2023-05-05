af49e0197a13aa9fc36df20b15c5c5ef9d1bb80c
GMCINTO <89421618+GMCINTO@users.noreply.github.com>
2023-05-05 14:26:51 -0600

    Update about.php

--- about.php
+++ about.php
@@ -1,37 +1,36 @@
-<meta charset="UTF-8">
-  <meta name="credit from W3schools about cards." content="About Page">
-  <meta name="keywords" content="HTML, CSS,">
-  <meta name="author" content="Jaimuel Silva">
+<html>
+
+
 <body>
-<h2>About Page</h2>
-<div class = "row">
-    <div class = "column">
-    <div class="card">
-            <?php echo Asset::img('IMG_0196.JPG', array('style' => 'width:100%')) ?>
-            <div class="container">
-                <h4><b>Jaimuel Silva</b></h4> 
-                <p>My name is Jaimuel Silva and I am studying computer science at Colorado State University. My goal is to develop a software that will impact the technology world. My biggest achievement was being able to tear down a car engine and put it back together with no issues.</p> 
-            </div>
+    <div class="center_abtpg">
+        <h1 style="color:blue">About Page</h1>
+    </div>
+    <div class="center_abtpg">
+        <?php
+        //use Fuel\Core\Asset;
+        
+        echo Asset::img('Austin-Picture.jpeg', array('style' => 'width:100%')) ?>
+        <div class="container">
+            <h4 style="color:blue"><b>Austin Persichetti</b></h4>
+            <p style="color:blue">My name is Austin Persichetti, I am a third year Colorado State University student studying
+                Computer Science with a minor in Business Administration. My main goals are to better myself and
+                learn the most I can to prepare for my future careeer.
+            <p>
         </div>
     </div>
-    <div class = "column">
-    <div class="card">
-            <?php echo Asset::img('Austin-Picture.JPG', array('style' => 'width:100%')) ?>
-            <div class="container">
-                <h4><b>Austin Persichetti</b></h4> 
-                <p>My name is Austin Persichetti, I am a third year Colorado State University student studying Computer Science with a minor in Business Administration. My main goals are to better myself and learn the most I can to prepare for my future career.</p> 
-            </div>
+    <div class="center_abtpg">
+        <?php
+        //use Fuel\Core\Asset;
+        echo Asset::img('selfie.jpg', array('style' => 'width:100%')) ?>
+        <div class="container">
+            <h4 style="color:yellow"><b>Glen</b></h4>
+            <p>
+            <p style="color:yellow">My name is Glen McIntosh, I am a third year Colorado State University student studying
+                Computer Science with a minor in Business Administration. I want to learn as much as I can so I
+                can have a fun and fulfilling career.
+            </p>
         </div>
     </div>
-    <div class = "column">
-    <div class="card">
-            <?php echo Asset::img('selfie.jpg', array('style' => 'width:100%')) ?>
-            <div class="container">
-                <h4><b>Glen McIntosh</b></h4> 
-                <p>My name is Glen McIntosh and I am studying computer science at Colorado State University. my goal is to not work alot and make alot of money.</p> 
-            </div>
-        </div>
-    </div>
-</div>
 </body>
-</html> 
+
+</html>
