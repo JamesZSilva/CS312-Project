164bc6ee396f3f561638f7e37fafbf977bc53425
AustinSketti <110070395+AustinSketti@users.noreply.github.com>
2023-05-05 16:58:19 -0600

    Update table.php

--- table.php
+++ table.php
@@ -1,13 +1,11 @@
 <!DOCTYPE HTML>
 <html>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-
 <head>
 </head>
-
 <body>
-    <h1> Table </h1>
-    <?php echo Form::open(['action' => 'https://cs.colostate.edu:4444/~glemicnt/m1/fuelviews/index.php/milestone/rowfunction', 'method' => 'post']); ?>
+<h1> Table </h1>
+    <?php echo Form::open(['action' => 'https://cs.colostate.edu:4444/~sketti/m1/fuelviews/index.php/milestone/rowfunction', 'method' => 'post']); ?>
     <?php echo Form::label('Enter Row Value: ', 'row_value'); ?>
     <?php echo Form::input('rowvalue'); ?>
     <br><br>
@@ -18,7 +16,7 @@
     <?php echo Form::close(); ?>
 
     <h1> Add a Color to the Database </h1>
-    <?php echo Form::open(['action' => 'https://cs.colostate.edu:4444/~glemicnt/m1/fuelviews/index.php/milestone/addcolor', 'method' => 'post']); ?>
+    <?php echo Form::open(['action' => 'https://cs.colostate.edu:4444/~sketti/m1/fuelviews/index.php/milestone/addcolor', 'method' => 'post']); ?>
     <?php echo Form::label('Enter Color Name: ', 'color_name'); ?>
     <?php echo Form::input('color_name'); ?>
     <br><br>
@@ -28,11 +26,22 @@
     <?php echo Form::submit('submit', 'Submit'); ?>
     <?php echo Form::close(); ?>
 
-   
-
     <?php if (isset($table)): ?>
         <?php echo $table; ?>
     <?php endif; ?>
+    
+    <script>
+        $(document).ready(function() {
+            $("#myForm").submit(function(e) {
+                var rowValue = $("#rowValueInput").val();
+                var colorValue = $("#colorValueInput").val();
+                
+                if (rowValue < 2 || rowValue > 26 || colorValue < 1 || colorValue > 10) {
+                    alert("Invalid input. Please enter a row value between 2 and 26 and a color value between 1 and 10.");
+                    e.preventDefault();
+                }
+            });
+        });
+    </script>
 </body>
-
 </html>
